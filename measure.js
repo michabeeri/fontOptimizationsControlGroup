53a647dcc794aedbd331147a9fe6c43734299ada
michabeeri <michaelb@wix.com>
2018-06-14 17:36:04 +0300

    include ssr status and actual size

--- measure.js
+++ measure.js
@@ -1,17 +1,28 @@
 require('console.table')
 const sites = require('./sites.json')
 const versionUtils = require('./versionUtils')
+const  fs = require('fs-extra');
 const exec = require('child_process').exec
+const {ungzip} = require('node-gzip');
 
 function formatSize(size) {
     var i = Math.floor( Math.log(size) / Math.log(1024) )
     return ( size / Math.pow(1024, i) ).toFixed(2) * 1 + ' ' + ['B', 'KB', 'MB'][i]
 }
 
-async function measureDocumentSizeOverTheWire(url) {
-    var command = `curl -H "Accept-Encoding: gzip, deflate, br" -s -o null -w "%{size_download}" ${url}`
-    var result = await new Promise((resolve, reject) => exec(command, (error, stdout, stderr) => error ? reject(error) : resolve(stdout)))
-    return parseInt(result, 10)
+async function downloadAndAnalizeHtml(siteName, url) {
+    const fileName = `htmlFiles/${siteName}.html`
+    const command = `curl -H "Accept-Encoding: gzip, deflate, br" -s -o "${fileName}" ${url}`
+    await new Promise((resolve, reject) => exec(command, (error, stdout, stderr) => error ? reject(error) : resolve()))
+
+    const buffer = await fs.readFile(fileName)
+    const decompressedBuffer = await ungzip(buffer)
+    const isClientRendered = decompressedBuffer.toString().indexOf('var warmupData =') === -1;
+    return {
+        size: buffer.length,
+        actualSize: decompressedBuffer.length,
+        isClientRendered
+    }
 }
 
 async function getUrlParams() {
@@ -25,18 +36,18 @@ async function getUrlParams() {
 }
 
 (async () => {
+    const urlParams = await getUrlParams()
     const measureResults = await Promise.all(sites.map(async siteParams => {
-        const urlParams = await getUrlParams()
         const url = `${siteParams.url}${urlParams}`
-        return Object.assign({}, siteParams, {
-            url,
-            size: await measureDocumentSizeOverTheWire(url)
-        })
+        const result = await downloadAndAnalizeHtml(siteParams.siteName, url)
+        return Object.assign({}, siteParams, result, {url})
     }))
 
     const formattedResults = measureResults
         .sort((a, b) => a.siteName.localeCompare(b.siteName))
-        .map(({siteName, size, url}) => [siteName, formatSize(size), url])
+        .map(({siteName, size, actualSize, isClientRendered, url}) => isClientRendered ?
+            [siteName, '-', '-', 'SSR Fail', url] :
+            [siteName, formatSize(size), formatSize(actualSize), '\u2713', url])
 
-    console.table(['Site', 'Html size (gzipped)', 'url'], formattedResults)
+    console.table(['Site', 'Compressed size', 'Actual size', 'SSR status', 'url'], formattedResults)
 })();
